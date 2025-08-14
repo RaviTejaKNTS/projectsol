c31d89c0db09643cc3a38b8c13185edf549266b9
RaviTejaKNTS <ravitejaknts@gmail.com>
2025-08-14 22:10:53 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// IMPORTANT:
+// If your GitHub repo name is not "tasksmint", change the base below to '/<your-repo-name>/'
 export default defineConfig({
+  base: '/tasksmint/',
   plugins: [react()],
 })
